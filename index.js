3f8ab2b66be580248b40ee798e967736a8e23da4
bhushank <bhushank@noitavonne.com>
2023-12-12 13:16:59 +0530

    7 Block scope and shadowing

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-// console.log(a); // index.js:1 Uncaught ReferenceError: Cannot access 'a' before initialization
-let a = 10;
+/* {
+  var a = 1;
+  let b = 2;
+  const c = 3;
+  console.log("{}", a, b, c);
+} */
 
-console.log(a);
+// var a = 1;
+// var b = 2;
+// {
+//   var a = 3; // shadows above a and b because it refers in global execution context
+//   var b = 4; //
+//   console.log("{13}", a, b);
+// }
+// console.log("15", a, b);
+
+/* let a = 1;
+const b = 2;
+{
+  let a = 3; // const and let will print different values for each block scope bcoz separete memory for each block scope allocation
+  const b = 4; //
+  console.log("{22}", a, b);
+}
+console.log("24", a, b); */
+
+/* let a = 1;
+{
+  var a = 2; // gives error because it reverse shadowing is not possible and same for const Uncaught SyntaxError: Identifier 'a' has already been declared (at index.js:28:7) this will be possible in function scope
+  console.log("{29}", a);
+} */
+
+// var a = 1;
+// {
+//   let a = 2; // this shadowing is possible
+//   console.log("{35}", a);
+// }
+
+/* let x = 1;
+function y() {
+  var x = 2; // this shadowing is possible
+  console.log("41", x);
+}
+y(); */
+
+// block scope follows lexical environment
+
+// scope rule for function works same for arrow functions

b06df0e76cd07da65a43409ba6e09b08aa26fb5a
bhushank <bhushank@noitavonne.com>
2023-12-13 15:13:58 +0530

    10  function

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-/* function a() {
-  for (var i = 1; i <= 5; i++) {
-    setTimeout(() => {
-      console.log(i);
-    }, i * 1000);
-  }
-  console.log(
-    "this will print 6 every time because all settime out referes to same i in memory"
-  );
-}
-a(); */
+a(); // regular works here
+// b(); // gives error message that b is not a function
 
-/* function b() {
-  for (let i = 1; i <= 5; i++) {
-    setTimeout(() => {
-      console.log(i);
-    }, i * 1000);
-  }
-  console.log(
-    "this will print 1,2 .. different every time because all settime out referes to different i in memory because i is blockscope here"
-  );
+// 1) function statement also known as function declaration
+function a() {
+  console.log("function a called");
 }
-b(); */
 
-function c() {
-  for (let i = 1; i <= 5; i++) {
-    function print(x) {
-      setTimeout(() => {
-        console.log(x);
-      }, x * 1000);
-    }
-    print(i);
-  }
+// 2) function expression
+var b = function c() {
+  console.log("function b(c) called");
+};
 
-  console.log(
-    "to achive output like b() then use function because var for every function different variable created"
-  );
+a(); //
+b();
+// c(); // gives index.js:16 Uncaught ReferenceError: c is not defined because it is not in outer scope or global scope
+
+// 3) both 1&2 is used to create function  but difference is in hoisting because b is a variable
+
+/* function (){
+  // anonymous function => this will give error should have name and cant 
+  use Uncaught SyntaxError: Function statements require a function name (at index.js:19:1)
 }
-c();
+ */
+
+var d = function () {
+  console.log("anonymous function called with function expression");
+};
+d();
+
+function e(param1, param2) {
+  // this is parameter and local access
+  console.log("param1: " + param1 + " param2: " + param2);
+}
+var arg1 = "arg1";
+var arg2 = "arg1";
+e(arg1, arg2); // this is argument
+
+// first class function means ability to send as argument or return or use as variable (not in JS, in other language also)
+
+// let and const is used as same above var but difference is in hoisting because let and const can be used after initialization
